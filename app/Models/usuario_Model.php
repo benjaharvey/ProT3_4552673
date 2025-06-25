bf238d2eb69ae86f7d3d156bdb9bccfc2d818639
benjaharvey <benjaharvey@gmail.com>
2025-06-25 04:49:59 -0300

    arme las tablas en phpmyadmin despues de tener 2 horas seguidas de errores, empiezo a armar los controladores y modelos, sigo mañana porque son las 5am

---
+++ app/Models/usuario_Model.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+
+class usuario_Model extends Model
+{
+    protected $table = 'usuarios';
+    protected $primaryKey = 'id_usuario';
+    protected $allowedFields = ['nombre_usuario', 'apellido_usuario', 'usuario', 'email_usuario', 'pass_usuario'. 'perfil_id', 'baja', 'created_at']
+}
